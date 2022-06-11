f043ac1e102b33de302992bb60038433f360e7d4
javlon <joeclaap@gmail.com>
2022-06-11 11:47:52 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import User from './components/User';
+import { BrowserRouter ,Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/user:id" element={<User />} />
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
